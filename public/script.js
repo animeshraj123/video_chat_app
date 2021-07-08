0dd4f6ad2b36fb81086c8a0c3fb2127de1ba532e
Animesh Raj <65447444+animeshraj123@users.noreply.github.com>
2021-07-08 17:37:44 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -23,8 +23,9 @@ const user = prompt("Enter your name");
 
 var peer = new Peer(undefined, {
   path: "/peerjs",
+  secure: true,
   host: "/",
-  port: "3030",
+  port: "443",
 });
 
 let myVideoStream;
